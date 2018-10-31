a20d687978759e5a91620b6d5b44001d52c7ad6b
luezgj <38480315+luezgj@users.noreply.github.com>
2018-10-30 23:29:40 -0300

    Add files via upload

--- src/org/example/www/gabragerecycler/GabrageRecyclerSkeleton.java
+++ src/org/example/www/gabragerecycler/GabrageRecyclerSkeleton.java
@@ -5,87 +5,85 @@
  * This file was auto-generated from WSDL
  * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
  */
-    package org.example.www.gabragerecycler;
-    /**
-     *  GabrageRecyclerSkeleton java skeleton for the axisService
-     */
-    public class GabrageRecyclerSkeleton{
-    	
-    	GarbageRecyclerModel model= new GarbageRecyclerModel();
-        
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param addRecyclingUser 
-             * @return addRecyclingUserResponse 
-         */
-        
-         public org.example.www.gabragerecycler.AddRecyclingUserResponse addRecyclingUser
-                  (
-                  org.example.www.gabragerecycler.AddRecyclingUser addRecyclingUser
-                  )
-        {
-        	 AddRecyclingUserResponse response= new AddRecyclingUserResponse(); 
-                //TODO : fill this with the necessary business logic
-        	 User user= model.findUser(addRecyclingUser.getUserName());
-        	 
-        	 if (user!=null) {
-        		 
-        	 }
-                return response;
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param registerUser 
-             * @return registerUserResponse 
-         */
-        
-                 public org.example.www.gabragerecycler.RegisterUserResponse registerUser
-                  (
-                  org.example.www.gabragerecycler.RegisterUser registerUser
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#registerUser");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param getAllRecyclingsByUser 
-             * @return getAllRecyclingsByUserResponse 
-         */
-        
-                 public org.example.www.gabragerecycler.GetAllRecyclingsByUserResponse getAllRecyclingsByUser
-                  (
-                  org.example.www.gabragerecycler.GetAllRecyclingsByUser getAllRecyclingsByUser
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getAllRecyclingsByUser");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param getRecyclingByUser 
-             * @return getRecyclingByUserResponse 
-         */
-        
-                 public org.example.www.gabragerecycler.GetRecyclingByUserResponse getRecyclingByUser
-                  (
-                  org.example.www.gabragerecycler.GetRecyclingByUser getRecyclingByUser
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getRecyclingByUser");
-        }
-     
-    }
-    
+package org.example.www.gabragerecycler;
+
+/**
+ * GabrageRecyclerSkeleton java skeleton for the axisService
+ */
+public class GabrageRecyclerSkeleton {
+
+	final static GarbageRecyclerModel model = new GarbageRecyclerModel();
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param addRecyclingUser
+	 * @return addRecyclingUserResponse
+	 */
+
+	public org.example.www.gabragerecycler.AddRecyclingUserResponse addRecyclingUser(
+			org.example.www.gabragerecycler.AddRecyclingUser addRecyclingUser) {
+		AddRecyclingUserResponse response = new AddRecyclingUserResponse();
+		// TODO : fill this with the necessary business logic
+		User user = model.findUser(addRecyclingUser.getUserName());
+
+		if (user != null) {
+			UserRecycling recycling = model.addUserRecycling(addRecyclingUser.getUserName(),addRecyclingUser.getUserRecycling());
+			response.setOut(recycling);
+		}
+		
+		return response;
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param registerUser
+	 * @return registerUserResponse
+	 */
+
+	public org.example.www.gabragerecycler.RegisterUserResponse registerUser(
+			org.example.www.gabragerecycler.RegisterUser registerUser) {
+		// TODO : fill this with the necessary business logic
+		RegisterUserResponse response= new RegisterUserResponse();
+		User registered=model.addUser(registerUser.getIn());
+		if (registered!=null)
+			response.setOut(registered);
+		return response;
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param getAllRecyclingsByUser
+	 * @return getAllRecyclingsByUserResponse
+	 */
+
+	public org.example.www.gabragerecycler.GetAllRecyclingsByUserResponse getAllRecyclingsByUser(
+			org.example.www.gabragerecycler.GetAllRecyclingsByUser getAllRecyclingsByUser) {
+		GetAllRecyclingsByUserResponse response= new GetAllRecyclingsByUserResponse();
+		String userName=getAllRecyclingsByUser.getUserName();
+		Recycling out = model.getAllRecyclingsByUser(userName);
+		response.setOut(out);
+		
+		return response;
+	}
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param getRecyclingByUser
+	 * @return getRecyclingByUserResponse
+	 */
+
+	public org.example.www.gabragerecycler.GetRecyclingByUserResponse getRecyclingByUser(
+			org.example.www.gabragerecycler.GetRecyclingByUser getRecyclingByUser) {
+		GetRecyclingByUserResponse response= new GetRecyclingByUserResponse();
+		String userName = getRecyclingByUser.getUserName();
+		for (UserRecycling usrRec : model.getRecyclingByUser(userName)) {
+			response.addUserRecyclings(usrRec);
+		}
+		return response;
+	}
+
+}
--- src/org/example/www/gabragerecycler/GarbageRecyclerModel.java
+++ src/org/example/www/gabragerecycler/GarbageRecyclerModel.java
@@ -1,14 +1,76 @@
 package org.example.www.gabragerecycler;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class GarbageRecyclerModel {
-	private Map<String,User> users= new HashMap();
-	private Map<String,List<UserRecycling>> userRecyclings = new HashMap();
+	private static int UserID = 0;
+	private static int RecyclingID = 0;
+	private Map<String, User> users = new HashMap<>();
+	private Map<String, List<UserRecycling>> userRecyclings = new HashMap<>();
+
+	public User findUser(String userName) {
+		return users.get(userName);
+	}
 	
-	findUser(){
+	public boolean userExists(String userName) {
+		if (findUser(userName)!=null) return true;
+		return false;
+	}
+	
+	public User addUser(User user) {
+		if (!userExists(user.getUserName())) {
+			user.setId(UserID);
+			UserID++;
+			users.put(user.getUserName(), user);
+			List<UserRecycling> recyclings = new ArrayList<>();
+			userRecyclings.put(user.getUserName(), recyclings);
+			return user;
+		}
+		return null;
+		//Generar errores
+	}
+
+	public UserRecycling addUserRecycling(String userName, UserRecycling recycling) {
+		User user = this.findUser(userName);
+		if (user != null) {
+			recycling.setId(RecyclingID);
+			RecyclingID++;
+			recycling.setUser(user);
+			recycling.setDate(new Date(System.currentTimeMillis()));
+			userRecyclings.get(userName).add(recycling);
+			return recycling;
+		}
+		return null;
+	}
+	
+	public Recycling getAllRecyclingsByUser(String userName) {
+		Recycling out= new Recycling();
+		if (userExists(userName)) {
+			for (UserRecycling usrRec : this.getRecyclingByUser(userName)) {
+				out.setBottles(usrRec.getBottles());
+				out.setPaperboard(usrRec.getPaperboard());
+				out.setGlass(usrRec.getGlass());
+				out.setCans(usrRec.getCans());
+				out.setTetrabriks(usrRec.getTetrabriks());
+			}
+			out.setTons(calculateTons(this.getRecyclingByUser(userName)));
+			return out;
+		}
+		//generar errores
+		return null;
 		
+	}
+	
+	public List<UserRecycling> getRecyclingByUser(String userName) {
+		return this.userRecyclings.get(userName);
+		
+	}
+	
+	private float calculateTons(List<UserRecycling> recyclings) {
+		return (float) 0.1; //para testear
 	}
 }
